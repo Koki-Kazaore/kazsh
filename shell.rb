af782c468083db1fe4d46fb4646af7a38eb0622d
Koki-Kazaore <kazaore@icloud.com>
2025-07-13 12:59:51 +0900

    Support for commands with arguments

--- shell.rb
+++ shell.rb
@@ -9,11 +9,19 @@ loop do
 
   # Read one line from standard input
   input = gets
-
-  # Ctrl+D (EOF) to exit
   break if input.nil?
 
-  # Execute command
-  command = input.chomp
-  system(command)
+  # Separate command and argument by whitespace
+  parts = input.chomp.split
+  command = parts[0]
+  args = parts[1..-1]
+
+  # Skip if no command is entered
+  next if command.nil? || command.empty?
+
+  # Start process
+  pid = spawn(command, *args)
+
+  # Wait for the process to finish
+  Process.wait(pid)
 end
